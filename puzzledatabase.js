f7c0f2b46a2a01b29ac11725eb08072012ade964
Sean <seanysean700@gmail.com>
2017-09-10 19:17:50 -0300

    Update puzzledatabase.js

--- puzzledatabase.js
+++ puzzledatabase.js
@@ -9,13 +9,15 @@ var puzzleNames = ["completely won","beginner 001","beginner 002","beginner 003"
                    "domination"];
 var candidateName = prompt("Enter puzzle name");
 var output = document.getElementById("output");
+var looping = true;
 for(i = 0; i < puzzleNames.length; i++) {
   console.log("i is " + i);
   console.log("Make sure puzzleNames is fine: " + puzzleNames[i]);
   if (puzzleNames[i] === candidateName.toLowerCase()) {
     output.innerHTML = `<a href="https://learnchess.neocities.org/puzzles/${i + 1}.html">Duplicate found</a>`;
+    looping = false;
   }
-  else {
+  else if (looping) {
     output.innerHTML = "No puzzle found with that name";
     console.log("Candidate " + candidateName);
   }
